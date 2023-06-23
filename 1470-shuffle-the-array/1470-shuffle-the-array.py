3d3dc957c825dcdb4a04e116e9bc98350e8c888d
Sun-Rang <114118523+Sun-Rang@users.noreply.github.com>
2023-06-23 15:48:53 +0900

    Time: 86 ms (6.21%), Space: 16.6 MB (27.88%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -1,9 +1,3 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        x1 = nums[0:n]
-        y1 = nums[n:]
-        new_nums = []
-        for i in range(n):
-            new_nums.append(x1[i])
-            new_nums.append(y1[i]) 
-        return new_nums
+        return [nums[i//2] if i % 2 == 0 else nums[i//2 + n] for i in range(2 * n)]
